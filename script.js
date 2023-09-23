e23bebddc991a0b41edb3fa46c3f99a20fd5bef3
yodit93 <yoditabebe9391@gmail.com>
2023-09-23 14:47:00 +0300

    Add dots functionality

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 const prev = document.querySelector('.prev-btn');
 const next = document.querySelector('.next-btn');
-const icon = document.querySelector('i');
 const slides = document.querySelectorAll('.carousel-slide');
 const container = document.querySelector('.carousel-container');
 let currentSlide = 0;
 const slidesDesktop = container.getAttribute('slides-display-d');
 const slidesTablet = container.getAttribute('slides-display-t');
 const slidesMobile = container.getAttribute('slides-display-m');
+const dots = document.querySelectorAll('.dot');
 let slideToShow = 0;
-if(screen.width <= 500) {
+if (screen.width <= 768) {
     slideToShow = parseInt(slidesMobile);
-}
-else if(screen.width > 500 && screen.width < 990) {
+} else if (screen.width > 768 && screen.width < 990) {
     slideToShow = parseInt(slidesTablet);
-}
-else if(screen.width >= 990) {
+} else if (screen.width >= 990) {
     slideToShow = parseInt(slidesDesktop);
 }
+const totalSlides = slides.length; // Total number of slides
+
 const slideShow = () => {
     slides.forEach((slide, index) => {
-        if(index >= currentSlide && index < currentSlide + slideToShow) {
+        const distance = (index - currentSlide + totalSlides) % totalSlides;
+        if (distance < slideToShow) {
             slide.style.display = 'block';
-        }
-        else {
+        } else {
             slide.style.display = 'none';
         }
     });
 };
+
 prev.addEventListener('click', () => {
-    if(currentSlide > 0) {
-        currentSlide = currentSlide - 1;
-        slideShow();
-    }   
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    slideShow();
 });
 
 next.addEventListener('click', () => {
-    if(currentSlide < slides.length - slideToShow) {
-        currentSlide = currentSlide + 1;
-        console.log(currentSlide)
-        slideShow();
-    };
+    currentSlide = (currentSlide + 1) % totalSlides;
+    slideShow();
 });
+
+dots.forEach((dot) => {
+    dot.addEventListener('click', () => {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        slideShow();
+    })
+})
+
+window.addEventListener('resize', () => {
+    if (screen.width <= 768) {
+        slideToShow = parseInt(slidesMobile);
+    } else if (screen.width > 768 && screen.width < 990) {
+        slideToShow = parseInt(slidesTablet);
+    } else if (screen.width >= 990) {
+        slideToShow = parseInt(slidesDesktop);
+    }
+    slideShow();
+});
+
 
 slideShow();
