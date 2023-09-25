664a0fa58c4e92cc3db80e7493559d399fb66d46
yodit93 <yoditabebe9391@gmail.com>
2023-09-25 18:48:25 +0300

    Fetch meals data with d/t category

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const prev = document.querySelector('.prev-btn');
 const next = document.querySelector('.next-btn');
-const slides = document.querySelectorAll('.carousel-slide');
 const container = document.querySelector('.carousel-container');
-let currentSlide = 0;
 const slidesDesktop = container.getAttribute('slides-display-d');
 const slidesTablet = container.getAttribute('slides-display-t');
 const slidesMobile = container.getAttribute('slides-display-m');
 const dots = document.querySelectorAll('.dot');
+
 let slideToShow = 0;
 if (screen.width < 600) {
     slideToShow = parseInt(slidesMobile);
@@ -15,46 +14,99 @@ if (screen.width < 600) {
 } else if (screen.width >= 990) {
     slideToShow = parseInt(slidesDesktop);
 }
-const totalSlides = slides.length; // Total number of slides
 
-const slideShow = () => {
-    slides.forEach((slide, index) => {
-        const distance = (index - currentSlide + totalSlides) % totalSlides;
-        if (distance < slideToShow) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
+const fetchData = async (endpoint) => {
+    try {
+        const response = await fetch(endpoint);
+        if(!response.ok) {
+            throw new Error("Network response was not ok")
         }
-    });
+        const data = await response.json();
+        return data;
+    }
+    catch(error) {
+        throw error;
+    }
 };
 
-prev.addEventListener('click', () => {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    slideShow();
-});
+const clearCarousel = () => {
+    const slides = document.querySelectorAll('.carousel-slide');
+    slides.forEach(slide => container.removeChild(slide));  
+}
 
-next.addEventListener('click', () => {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    slideShow();
-});
+const fetchCategory = (category) => {
+    let currentSlide = 0;
+    let recipes = [];
+    fetchData(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`).then(data => {
+        recipes = data.meals;
+        recipes.map((recipe) => {
+            const slide = document.createElement('div');
+            slide.className = "carousel-slide";
+            const image = document.createElement('img');
+            image.src = recipe.strMealThumb;
+            image.alt = "image";
+            image.className = "slideImg";
+            slide.appendChild(image);
+            container.appendChild(slide);
+        });
 
-dots.forEach((dot) => {
-    dot.addEventListener('click', () => {
-        currentSlide = (currentSlide + 1) % totalSlides;
+        const slides = document.querySelectorAll('.carousel-slide');
+        const totalSlides = slides.length;
+        const slideShow = () => {
+            slides.forEach((slide, index) => {
+                const distance = (index - currentSlide + totalSlides) % totalSlides;
+                if (distance < slideToShow) {
+                    slide.style.display = 'block';
+                } else {
+                    slide.style.display = 'none';
+                }
+            });
+        };
+
+        prev.addEventListener('click', () => {
+            currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+            slideShow();
+        });
+
+        next.addEventListener('click', () => {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            slideShow();
+        });
+
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                currentSlide = index;
+                slideShow();
+            })
+        })
+
+        window.addEventListener('resize', () => {
+            if (screen.width < 600) {
+                slideToShow = parseInt(slidesMobile);
+            } else if (screen.width >= 600 && screen.width < 990) {
+                slideToShow = parseInt(slidesTablet);
+            } else if (screen.width >= 990) {
+                slideToShow = parseInt(slidesDesktop);
+            }
+            slideShow();
+        });
+
+
         slideShow();
+
+        
+        })
+        .catch(err => err);
+}
+
+const navLinks = document.querySelectorAll('.recipe-list a');
+navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        clearCarousel();
+        e.preventDefault();
+        const category = link.textContent;
+        fetchCategory(category);
     })
 })
 
-window.addEventListener('resize', () => {
-    if (screen.width < 600) {
-        slideToShow = parseInt(slidesMobile);
-    } else if (screen.width >= 600 && screen.width < 990) {
-        slideToShow = parseInt(slidesTablet);
-    } else if (screen.width >= 990) {
-        slideToShow = parseInt(slidesDesktop);
-    }
-    slideShow();
-});
-
-
-slideShow();
+fetchCategory("Starter");
